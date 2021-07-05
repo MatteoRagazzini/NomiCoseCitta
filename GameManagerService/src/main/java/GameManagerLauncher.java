d30c0ebe8c4280f364b827d36de293b194df027c
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2021-07-05 18:10:42 +0200

    change name main class

--- GameManagerService/src/main/java/GameManagerLauncher.java
+++ GameManagerService/src/main/java/GameManagerLauncher.java
@@ -1,7 +1,7 @@
 import model.game.GameManager;
 import model.round.RoundManager;
 
-public class ServicesLauncher {
+public class GameManagerLauncher {
     public static void main(String[] args) {
        new GameManager();
     }
